cdf1144b637b28339a061a2b180430e6e5379c34
FUJI Goro (gfx) <gfuji@cpan.org>
2016-07-23 18:42:24 +0900

    CLI

---
+++ src/cli.ts
@@ -0,0 +1,37 @@
+
+import * as main from "./index";
+
+function die(message: string) {
+    console.error(message);
+    process.exit(1);
+}
+
+const options: main.Options = {
+    githubAccessToken: null,
+};
+
+const args = process.argv.splice(2);
+if (args.length === 0) {
+    die(`usage: ${process.argv[1]} --token $GITHUB_ACCESS_TOKEN`);
+}
+for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (arg === "-t" || arg === "--token") {
+        if (++i === args.length) {
+            die(`No value for ${arg}`);
+        }
+        options.githubAccessToken = args[i];
+    }
+}
+
+main.start(options).then((pullRequestUrl) => {
+    console.log("Successfully creqted a pull-request: %s", pullRequestUrl);
+}).catch((reason) => {
+    // handle expected reasons
+    if (reason instanceof main.AllDependenciesAreUpToDate) {
+        console.log("All the dependencies are up to date.");
+        return;
+    }
+    // unexpected reasons are exceptions
+    throw reason;
+});
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { GitHubApi, GitHubPullRequestParameters } from "./github";
 import { exec } from "child_process";
 import * as moment from "moment";
 
-const GITHUB_ACCESS_TOKEN = process.argv[1];
-
 function run(command: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
         console.log(`>> ${command}`);
@@ -25,67 +23,62 @@ function run(command: string): Promise<string> {
     });
 }
 
-class AllDependenciesAreUpToDate { }
+export class AllDependenciesAreUpToDate { }
 
-ShrinkWrap.read().then((shrinkWrap) => {
-    return shrinkWrap.getLatest();
-}).then((packageInfoList) => {
-    const outdatedList = packageInfoList.filter((packageInfo) => {
-        return packageInfo.isOutdated();
+export type Options = {
+    githubAccessToken: string,
+}
+
+export function start({githubAccessToken: githubAccessToken}: Options): Promise<string> {
+    return ShrinkWrap.read().then((shrinkWrap) => {
+        return shrinkWrap.getLatest();
+    }).then((packageInfoList) => {
+        const outdatedList = packageInfoList.filter((packageInfo) => {
+            return packageInfo.isOutdated();
+        });
+
+        if (outdatedList.length === 0) {
+            return Promise.reject(new AllDependenciesAreUpToDate());
+        }
+        console.log("");
+
+        const issue = Issue.create(outdatedList);
+        console.log(issue);
+        const timestamp = moment().format("YYYY-MM-DD-hh-mm-ss");
+
+        const branch = `npm-update/${timestamp}`;
+
+        return run(`git checkout -b ${branch}`).then((_result) => {
+            return run("npm update --depth 9999");
+        }).then((_result) => {
+            return run("npm shrinkwrap");
+        }).then((_result) => {
+            return run("git add npm-shrinkwrap.json");
+        }).then((_result) => {
+            return run("git commmit -m 'npm update --depth 9999'");
+        }).then((_result) => {
+            return run("git push origin HEAD");
+        }).then((_result) => {
+            return run("git checkout -");
+        }).then((_result) => {
+            return run("git rev-parse --abbrev-ref HEAD");
+        }).then((baseBranch) => {
+            return Promise.all([
+                run("git remote get-url --push origin"),
+                Promise.resolve({
+                    title: `npm update at ${new Date()}`,
+                    body: issue,
+                    head: branch,
+                    base: baseBranch,
+                }),
+            ]);
+        });
+    }).then(([repositoryUrl, pullRequestData]: [string, GitHubPullRequestParameters]) => {
+        return new GitHubApi({
+            repositoryUrl: repositoryUrl,
+            token: githubAccessToken,
+        }).createPullRequest(pullRequestData);
+    }).then((response) => {
+        return Promise.resolve(response.html_url);
     });
-
-    if (outdatedList.length === 0) {
-        return Promise.reject(new AllDependenciesAreUpToDate());
-    }
-    console.log("");
-
-    const issue = Issue.create(outdatedList);
-    console.log(issue);
-    const timestamp = moment().format("YYYY-MM-DD-hh-mm-ss");
-
-    const branch = `npm-update/${timestamp}`;
-
-    return run(`git checkout -b ${branch}`).then((_result) => {
-        return run("npm update --depth 9999");
-    }).then((_result) => {
-        return run("npm shrinkwrap");
-    }).then((_result) => {
-        return run("git add npm-shrinkwrap.json");
-    }).then((_result) => {
-        return run("git commmit -m 'npm update --depth 9999'");
-    }).then((_result) => {
-        return run("git push origin HEAD");
-    }).then((_result) => {
-        return run("git checkout -");
-    }).then((_result) => {
-        return run("git rev-parse --abbrev-ref HEAD");
-    }).then((baseBranch) => {
-        return Promise.all([
-            run("git remote get-url --push origin"),
-            Promise.resolve({
-                title: `npm update at ${new Date()}`,
-                body: issue,
-                head: branch,
-                base: baseBranch,
-            }),
-        ]);
-    });
-}).then((params: [string, GitHubPullRequestParameters]) => {
-    // FIXME: parameter destructuring
-    const repositoryUrl = params[0];
-    const pullRequestData = params[1];
-    return new GitHubApi({
-        repositoryUrl: repositoryUrl,
-        token: GITHUB_ACCESS_TOKEN,
-    }).createPullRequest(pullRequestData);
-}).then((response) => {
-    console.log("Successfully creqted a pull-request: %s", response.html_url);
-}).catch((err) => {
-
-    if (err instanceof AllDependenciesAreUpToDate) {
-        console.log("All the dependencies are up to date.");
-        return;
-    }
-
-    throw err;
-});
+}
