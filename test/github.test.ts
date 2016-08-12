bf6ff574bf900396efdfb8612fce6134135a260f
FUJI Goro (gfx) <gfuji@cpan.org>
2016-08-12 11:20:39 +0900

    add tests

--- test/github.test.ts
+++ test/github.test.ts
@@ -1,5 +1,6 @@
 import * as assert from "power-assert";
 import { GitHubApi } from "../src/github";
+import { NpmConfig } from "../src/npm_config";
 
 const githubDataSet = {
     "github ssh url": "git@github.com:foo/something.useful.git",
@@ -12,39 +13,45 @@ const gheDataSet = {
     "ghe https+git url": "https://ghe.example.com/foo/something.useful.git",
 };
 describe("GitHubApi", () => {
-    context("github.com", () => {
+    context(".parseUrl(reading github.com URLs)", () => {
         Object.keys(githubDataSet).forEach((name) => {
             const url = <string>(<any>githubDataSet)[name];
 
-            it(`extract "endpoint" from ${name}`, () => {
+            it(`extracts "endpoint" from ${name}`, () => {
                 assert(GitHubApi.extractEndpoint(url) === "https://api.github.com");
             });
 
-            it(`extract "owner" from ${name}`, () => {
+            it(`extracts "owner" from ${name}`, () => {
                 assert(GitHubApi.extractOwner(url) === "foo");
             });
 
-            it(`extract "repository" from ${name}`, () => {
+            it(`extracts "repository" from ${name}`, () => {
                 assert(GitHubApi.extractRepository(url) === "something.useful");
             });
         });
     });
 
-    context("ghe", () => {
+    context(".parseUrl(ghe URLs)", () => {
         Object.keys(gheDataSet).forEach((name) => {
             const url = <string>(<any>gheDataSet)[name];
 
-            it(`extract "endpoint" from ${name}`, () => {
+            it(`extracts "endpoint" from ${name}`, () => {
                 assert(GitHubApi.extractEndpoint(url) === "https://ghe.example.com/api/v3");
             });
 
-            it(`extract "owner" from ${name}`, () => {
+            it(`extracts "owner" from ${name}`, () => {
                 assert(GitHubApi.extractOwner(url) === "foo");
             });
 
-            it(`extract "repository" from ${name}`, () => {
+            it(`extracts "repository" from ${name}`, () => {
                 assert(GitHubApi.extractRepository(url) === "something.useful");
             });
+        });
+    });
+    context(".parseUrl(package.json)", () => {
+        it(`can parse itself`, () => {
+            const npmConfig = new NpmConfig(require("../package.json"));
+            assert(GitHubApi.extractRepository(npmConfig.repository.url) === "ci-npm-update");
         });
     });
 });
